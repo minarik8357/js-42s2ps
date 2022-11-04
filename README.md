f54c9f5e3e87315925e90a185bcbbcfd91cd4e5d
minarik8357 <113665355+minarik8357@users.noreply.github.com>
2022-11-04 15:15:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-42s2ps
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-42s2ps)
---
+++ index.js
@@ -0,0 +1,6 @@
+
+
+
+const pole = [1,4,6,3]
+console.log(pole)
+console.log(pole[0] + pole[1] + pole[2] + pole[3])
